b6ae8d98e0c50a07505a6e572a486e66258d9c9b
hkldtrinanda <hkldtrinanda12@gmail.com>
2021-09-23 23:39:22 +0700

    Pong-Revisi

---
+++ Pong_Revisi_Project/Assets/BallControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float xInitialForce;
+    public float yInitialForce;
+    private Vector2 trajectoryOrigin;
+
+    // Start is called before the first frame update
+
+void PushBall()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rb2d.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+    void Start()
+    {
+    rb2d = GetComponent<Rigidbody2D>();
+    Invoke("PushBall", 2);
+    trajectoryOrigin = transform.position;
+    }
+
+    void ResetBall(){
+    rb2d.velocity = Vector2.zero;
+    transform.position = Vector2.zero;
+    }
+
+    void RestartGame(){
+    ResetBall();
+    Invoke("Pushball", 1);
+}
+
+    void OnCollisionEnter2D (Collision2D coll) {
+    if(coll.collider.CompareTag("Player")){
+        Vector2 vel;
+        vel.x = rb2d.velocity.x;
+        vel.y = (rb2d.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+        rb2d.velocity = vel;
+    }
+}
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+        public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+    
+}
---
+++ Pong_Revisi_Project/Assets/PlayerControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+    private ContactPoint2D lastContactPoint;
+    // Skor pemain
+    private int score;
+
+    // Untuk mengakses informasi titik kontak dari kelas lain
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    // Ketika terjadi tumbukan dengan bola, rekam titik kontaknya.
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dapatkan kecepatan raket sekarang.
+        Vector2 velocity = rb2d.velocity;
+
+        // Jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas).
+        if (Input.GetKey(moveUp))
+        {
+            velocity.y = speed;
+        }
+
+        // Jika pemain menekan tombol ke bawah, beri kecepatan negatif ke komponen y (ke bawah).
+        else if (Input.GetKey(moveDown))
+        {
+            velocity.y = -speed;
+        }
+
+        // Jika pemain tidak menekan tombol apa-apa, kecepatannya nol.
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rb2d.velocity = velocity;
+        var pos = transform.position;
+    if (pos.y > boundY) {
+        pos.y = boundY;
+    }
+    else if (pos.y < -boundY) {
+        pos.y = -boundY;
+    }
+    transform.position = pos;
+    }
+    
+    // Menaikkan skor sebanyak 1 poin
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    // Mengembalikan skor menjadi 0
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    // Mendapatkan nilai skor
+    public int Score
+    {
+        get { return score; }
+    }
+}
